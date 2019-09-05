b9383cf567e7aeac3fa3f263f660f8c439cd9383
Runs <jetherruns@gmail.com>
2019-09-05 15:41:33 +0530

    user model session post model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  
+  def show
+  	@user = User.find(params[:id])
+  	@posts = @user.posts
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		log_in @user
+  		flash[:success] = "Welcome, Adios!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+	  def user_params
+	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  end
+
+	  def correct_user
+	  	@user = User.find(params[:id])
+	  	redirect_to(root_url) unless current_user?
+	  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	root 	 'home#index'
+	get 	 '/home', to: 'home#index'
+  get 	 'sessions/new'
+  get 	 '/signup', to: 'users#new'
+  get 	 'login', to: 'sessions#new'
+  post 	 '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users
+  resources :posts, only: [:create, :destroy]
 end
